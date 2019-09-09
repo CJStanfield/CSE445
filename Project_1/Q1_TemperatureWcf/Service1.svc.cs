3ec7deaaa0a71b27a1688c85a6d463764a19a713
Cole Stanfield (Student) <cjstanfi@sundevils.asu.edu>
2019-09-08 19:45:01 -0700

    Added Comments for Q1

--- Project_1/Q1_TemperatureWcf/Service1.svc.cs
+++ Project_1/Q1_TemperatureWcf/Service1.svc.cs
@@ -12,29 +12,33 @@ namespace Project_1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-
+        //This first service contains some tutorial exampls along with the required functions
+        //Service returns simple string
         public string Hello()
         {
             return "Hello World";
         }
+        //service  returns pi
         public double PiValue()
         {
             double pi = Math.PI;
             return pi;
         }
 
+        //service returns the absolute value of a number
         public int absValue(int x)
         {
             if (x >= 0) return x;
             else return (-x);
         }
-
+        
+        //Service returns the conversion from celsius to fahrenheit. function found online
         public int c2f(int cel)
         {
             double fahrenheit = (cel * 1.8) + 32;
             return ((int)fahrenheit);
         }
-
+        //Service returns the conversion from fahrenheit to celsius. function found online
         public int f2c(int fahr)
         {
             double celsius = (fahr - 32) / 1.8;
