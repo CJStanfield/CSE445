8344d32497d2e8765aab84e8497584601036df4d
Cole Stanfield (Student) <cjstanfi@sundevils.asu.edu>
2019-09-16 21:36:53 -0700

    Set up project 2 classes

---
+++ Project_2/Project_2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Project_2
+{
+    public delegate void priceCutEvent(Int32 pr); //define a delegate
+    public class ChickenFarm
+    {
+        static Random rng = new Random();
+        public static event priceCutEvent priceCut;
+        private static Int32 chickenPrice = 10;
+        public Int32 getPrice() { return chickenPrice; }
+        public static void changePrice (Int32 price)
+        {
+            if(price < chickenPrice)
+            {
+                if (priceCut != null)
+                {
+                    priceCut(price);
+                }
+                chickenPrice = price;
+            }
+        }
+
+        public void farmerFunc()
+        {
+            for(Int32 i = 0; i < 50; i++)
+            {
+                Thread.Sleep(500);
+                Int32 p = rng.Next(5, 10);
+                Console.WriteLine("new price is {0}", p);
+                ChickenFarm.changePrice(p);
+            }
+        }
+    }
+
+    public class Retailer
+    {
+        public void retailerFunc()
+        {
+            ChickenFarm chicken = new ChickenFarm();
+            for (Int32 i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Int32 p = chicken.getPrice();
+                Console.WriteLine("Store{0} has everyday low price: ${1} each", Thread.CurrentThread.Name, p); //prints the name
+
+            }
+        }
+
+        public void chickenOnSale(Int32 p)
+        {
+            //Order chickens from chicken farm - send order into queue
+            Console.WriteLine("Store{0} chickens are on sale: as low as ${1} each", Thread.CurrentThread.Name, p);
+        }
+
+    }
+
+    public class myApplication
+    {
+        static void Main(string[] args)
+        {
+            ChickenFarm chicken = new ChickenFarm();
+            Thread farmer = new Thread(new ThreadStart(chicken.farmerFunc));
+            farmer.Start();         //Start one farmer thread
+            Retailer chickenstore = new Retailer();
+            ChickenFarm.priceCut += new priceCutEvent(chickenstore.chickenOnSale);
+            Thread[] retailers = new Thread[3];
+            for (Int32 i = 0; i < 3; i++)
+            {
+                //Start N retailer threads
+                retailers[i] = new Thread(new ThreadStart(chickenstore.retailerFunc));
+                retailers[i].Name = (i + 1).ToString();
+                retailers[i].Start();
+            }
+        }
+    }
+
+}
---
+++ Project_2/e-business/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace e_business
+{
+    public delegate void priceCutEvent(int pr);     //define a delegate
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public class Airline
+    {
+        private int ticketPrice;
+        public int getPrice() { return ticketPrice; }
+        public void airlineFunc()
+        {
+            for(int i = 0; i < 50; i++)
+            {
+                Thread.Sleep(500);
+            }
+        }
+    }
+
+    public class PricingModel
+    {
+
+    }
+
+    public class Buffer
+    {
+
+    }
+
+    public class OrderProcessing
+    {
+
+    }
+
+    public class TravelAgency
+    {
+
+
+    }
+}
